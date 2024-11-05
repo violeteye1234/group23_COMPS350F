6cac40afe90aeff2493636212598fe07868e10bf
OWOEOMO <50674595+OWOEOMO@users.noreply.github.com>
2024-11-06 03:34:46 +0800

    create a tkinter_designer_refactor.py to handle tkinter designer output
    I don't want to do the regex substitution with my bear hand anymore so i wrote this script to auto apply the regex to the gui.py that tkinter designer generated.

--- app/application/utils/page_controller.py
+++ app/application/utils/page_controller.py
@@ -17,3 +17,7 @@ class PageController:
     def cleanup(self) -> None:
         self.view.destroy()
         self.view = None # type: ignore
+        
+    def scale_all_widgets(self, scale_factor):
+        self.view.canvas.scale("all", 0, 0, scale_factor, scale_factor)  # 縮小比例
+        self.view.canvas.config(scrollregion=self.view.canvas.bbox("all"))  # 更新畫布的範圍
---
+++ tools/tkinter_designer_refactor.py
@@ -0,0 +1,32 @@
+import re
+import os
+
+# 定義目錄路徑
+filename = r"tkinter_designer_output\build\gui.py"
+
+# 定義正則表達式和替換模式
+patterns = [
+    (r'(image_image_.*) = PhotoImage\(\s*file=relative_to_assets\((.*)\)\)', r'\1 = PhotoImage(file=relative_to_assets(\2))'),
+    (r'(image_.*) = canvas.create_image\(\s*(.*),\s*(.*),\s*image=(.*)\s*\)', r'\1 = canvas.create_image(\2, \3, image=\4)'),
+    (r'canvas.create_text\(\s*(.*),\s*(.*),\s*anchor=(.*),\s*text=(.*),\s*fill=(.*),\s*font=(.*)\s*\)', r'canvas.create_text(\1, \2, anchor=\3, text=\4, fill=\5, font=\6)'),
+    (r'=relative_to_assets\("(.*)"\)', r'=self.image_path / "\1"'),
+    (r'image_(.*) =', r'self.image_\1 ='),
+    (r'=image_image_(.*)\)', r'=self.image_image_\1)'),
+    (r'canvas', r'self.canvas')
+]
+
+
+# 處理每個文件
+if filename.endswith(".py"):
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+    
+    # 對每個正則表達式進行替換
+    for pattern, replacement in patterns:
+        content = re.sub(pattern, replacement, content)
+    
+    # 寫回文件
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(content)
+
+print("所有文件已完成替換處理。")
