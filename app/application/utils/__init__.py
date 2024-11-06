a5cfc40a4ce9de88f9e5c33d1f38edce26d79f21
OWOEOMO <50674595+OWOEOMO@users.noreply.github.com>
2024-11-06 21:59:42 +0800

    remove resizable_canvas
    I dont know why is it here. i dont know what is this.

--- app/application/utils/__init__.py
+++ app/application/utils/__init__.py
@@ -1,5 +1,4 @@
 from .canvas_button import CanvasButton
 from .page_controller import PageController
 from .page_view import PageView
-from .resizable_canvas import ResizableCanvas
 from .scrollable_frame import ScrollableFrame
