fd454e31113631b006d12a1b5d81251e3a2d2eef
kkkkkeven <1208914246@qq.com>
2024-11-09 23:46:10 +0800

    kk
    Add mouseover events to canvas

--- app/application/pages/notification_center/notification_center_view.py
+++ app/application/pages/notification_center/notification_center_view.py
@@ -12,27 +12,28 @@ class NotificationCenterPageView(PageView):
         self.canvas.pack(fill="both", expand=True)
         self.current_content_controller = None
         
+
         self.image_image_1 = PhotoImage(file=self.image_path / "image_1.png")
         self.image_1 = self.canvas.create_image(150, 40, image=self.image_image_1)
 
         original_image_path2 = self.image_path / "image_2.png"
         desired_size2 = (800, 100)  # 期望的图片大小
         original_image2 = Image.open(original_image_path2)
-        resized_image = original_image2.resize(desired_size2)
+        resized_image2 = original_image2.resize(desired_size2)
 
         original_image_path3 = self.image_path / "image_3.png"
         desired_size3 = (800, 100)  # 期望的图片大小
         original_image3 = Image.open(original_image_path3)
-        resized_image = original_image3.resize(desired_size3)
+        resized_image3 = original_image3.resize(desired_size3)
 
         # 覆盖原始图片
-        resized_image.save(original_image_path2)
-        resized_image.save(original_image_path3)
+        resized_image2.save(original_image_path2)
+        resized_image3.save(original_image_path3)
 
     def render(self):
         from pages.main_page.main_page_controller import MainPageController
-        button_1 = CanvasButton(self.canvas, 420, 120, self.image_path / "image_3.png",lambda: MainPageController.switch_page(self,"my_baggage"))
-        button_2 = CanvasButton(self.canvas, 420, 220, self.image_path / "image_3.png",lambda: MainPageController.switch_page(self,"my_baggage"))
+        button_1 = CanvasButton(self.canvas, 420, 120, self.image_path / "image_3.png",lambda: MainPageController.switch_page(self,"my_baggage"),self.image_path / "image_2.png")
+        button_2 = CanvasButton(self.canvas, 420, 220, self.image_path / "image_3.png",lambda: MainPageController.switch_page(self,"my_baggage"),self.image_path / "image_2.png")
         self.parent.set_frame(self)
 
     def update(self):
--- app/application/utils/canvas_button.py
+++ app/application/utils/canvas_button.py
@@ -3,17 +3,29 @@ import tkinter as tk
 class CanvasButton:
     flash_delay = 100  # Milliseconds.
 
-    def __init__(self, canvas, x, y, image_path, command, state=tk.NORMAL, tags=None):
+    def __init__(self, canvas, x, y, image_path_normal, command,image_path_hover=None, state=tk.NORMAL, tags=None):
         self.canvas = canvas
-        self.btn_image = tk.PhotoImage(file=image_path)
+        self.btn_image_normal = tk.PhotoImage(file=image_path_normal)
+        self.btn_image_hover = tk.PhotoImage(file=image_path_hover) if image_path_hover else None
         self.canvas_btn_img_obj = canvas.create_image(x, y, anchor='center', state=state, 
-                                                      image=self.btn_image, tags=tags)
-        canvas.tag_bind(self.canvas_btn_img_obj, "<ButtonRelease-1>", 
-                        lambda event: (self.flash(), command()))
-        
+                                                      image=self.btn_image_normal, tags=tags)
+        canvas.tag_bind(self.canvas_btn_img_obj, "<ButtonRelease-1>", lambda event: (self.flash(), command()))
+        self.canvas.tag_bind(self.canvas_btn_img_obj, "<Enter>", self.on_enter)
+        self.canvas.tag_bind(self.canvas_btn_img_obj, "<Leave>", self.on_leave)
+
+
     def flash(self):
         self.set_state(tk.HIDDEN)
         self.canvas.after(self.flash_delay, self.set_state, tk.NORMAL)
 
     def set_state(self, state):
         self.canvas.itemconfigure(self.canvas_btn_img_obj, state=state)
+
+    def on_enter(self, event):
+        self.is_hovering = True
+        if self.btn_image_hover:
+            self.canvas.itemconfig(self.canvas_btn_img_obj, image=self.btn_image_hover)
+
+    def on_leave(self, event):
+        self.is_hovering = False
+        self.canvas.itemconfig(self.canvas_btn_img_obj, image=self.btn_image_normal)
