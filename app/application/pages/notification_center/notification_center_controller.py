1ad2d7819e1680dfd50dfff3f9eea58adf2842ed
Lipenncom1 <148969818+Lipenncom1@users.noreply.github.com>
2024-11-11 00:35:11 +0800

    fix switch page bug

--- app/application/pages/notification_center/notification_center_controller.py
+++ app/application/pages/notification_center/notification_center_controller.py
@@ -1,12 +1,13 @@
 from utils.page_controller import PageController
 from .notification_center_view import NotificationCenterPageView
+from pages.my_baggage.my_baggage_controller import MyBaggagePageController
+#from utils.scrollable_frame import ScrollableFrame
+#import tkinter as tk
+
 
 class NotificationCenterPageController(PageController):
     def __init__(self, root, parent_container):
         super().__init__(root, parent_container)
+        self.root = root
         self.view = NotificationCenterPageView(parent_container)
         self.view_set_controller()
-
-
-    #def go_to_my_baggage(self):
-       # self.root.show_page('my_baggage')
--- app/application/pages/notification_center/notification_center_view.py
+++ app/application/pages/notification_center/notification_center_view.py
@@ -3,6 +3,7 @@ from tkinter import PhotoImage
 from utils.page_view import PageView
 from utils.canvas_button import CanvasButton
 from PIL import Image
+#from utils.scrollable_frame import ScrollableFrame
 
 class NotificationCenterPageView(PageView):
     def __init__(self, parent):
@@ -10,9 +11,12 @@ class NotificationCenterPageView(PageView):
         self.image_path = self.image_path / "notification_center/images/"
         self.canvas = tk.Canvas(self, bg="#F5F5F5", height=712.5, width=892.5, bd=0, highlightthickness=0, relief="ridge")
         self.canvas.pack(fill="both", expand=True)
-        self.current_content_controller = None
+        #self.current_content_controller = None
         
 
+        
+
+    def render(self):
         self.image_image_1 = PhotoImage(file=self.image_path / "image_1.png")
         self.image_1 = self.canvas.create_image(150, 40, image=self.image_image_1)
 
@@ -20,22 +24,26 @@ class NotificationCenterPageView(PageView):
         desired_size2 = (800, 100)  # 期望的图片大小
         original_image2 = Image.open(original_image_path2)
         resized_image2 = original_image2.resize(desired_size2)
+        resized_image2.save(original_image_path2) # 覆盖原始图片
 
         original_image_path3 = self.image_path / "image_3.png"
         desired_size3 = (800, 100)  # 期望的图片大小
         original_image3 = Image.open(original_image_path3)
         resized_image3 = original_image3.resize(desired_size3)
+        resized_image3.save(original_image_path3) # 覆盖原始图片
 
-        # 覆盖原始图片
-        resized_image2.save(original_image_path2)
-        resized_image3.save(original_image_path3)
 
-    def render(self):
-        button_1 = CanvasButton(self.canvas, 420, 120, self.image_path / "image_3.png",
-                                lambda: self.controller.go_to_my_baggage(),self.image_path / "image_2.png")
-        button_2 = CanvasButton(self.canvas, 420, 220, self.image_path / "image_3.png",
-                                lambda: self.controller.go_to_my_baggage(),self.image_path / "image_2.png")
+        #button_1 = CanvasButton(self.canvas, 420, 120, self.image_path / "image_3.png",
+                               # lambda: self.controller.go_to_my_baggage(),self.image_path / "image_2.png")
+        button_1 = CanvasButton(self.canvas, 420, 120, self.image_path / "image_3.png",lambda: self.controller.root.page_controller.switch_page("my_baggage"),self.image_path / "image_2.png")
+        #button_2 = CanvasButton(self.canvas, 420, 220, self.image_path / "image_3.png",
+                               # lambda: self.controller.go_to_my_baggage(),self.image_path / "image_2.png")
         self.parent.set_frame(self)
 
+        
+
     def update(self):
+        #self.content_frame = ScrollableFrame(self.canvas, width=892.5, height=712.5, bg="#FFF0F0")
+        #self.content_frame.pack_propagate(False)
+        #self.canvas.create_window(187.5, 55.5, window=self.content_frame, anchor='nw')
         pass
