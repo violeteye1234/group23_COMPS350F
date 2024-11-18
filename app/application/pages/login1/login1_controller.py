a1ce84065ead2cc8e1b0bed1fc45fdfd8d04a049
terry2778 <s1303276@live.hkmu.edu.hk>
2024-11-18 16:26:23 +0800

    Update login1_controller.py

--- app/application/pages/login1/login1_controller.py
+++ app/application/pages/login1/login1_controller.py
@@ -1,5 +1,6 @@
 from utils.page_controller import PageController
 from .login1_view import Login1PageView
+from models.user_model import UserModel  # 导入UserModel
 import tkinter as tk
 from tkinter import messagebox
 
@@ -9,15 +10,17 @@ class Login1PageController(PageController):
         self.view = Login1PageView(parent_container)
         self.view.set_controller(self)
         self.view.render()
+        
+        # 创建UserModel实例
+        self.user_model = UserModel()
 
     def login(self, email: str, password: str):
         self.root.logger.info(f"Attempting to login with email: {email}, password: {password}")
         
-        # 固定的电子邮件和密码
-        correct_email = "123"
-        correct_password = "123"
+        # 从数据库中检索用户信息
+        user = self.user_model.get_user_by_email_and_password(email, password)
         
-        if email == correct_email and password == correct_password:
+        if user:
             self.root.logger.info("Login successful!")
             # 这里添加跳转到主页面的逻辑
             self.root.show_page('Main')
@@ -35,3 +38,4 @@ class Login1PageController(PageController):
     def cleanup(self):
         self.view.destroy()
         self.view = None
+        self.user_model.close()
