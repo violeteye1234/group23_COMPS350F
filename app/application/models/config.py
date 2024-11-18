a77b49f2dfaf98a7bc94835debe7eec501e6521a
terry2778 <s1303276@live.hkmu.edu.hk>
2024-11-19 03:02:15 +0800

    Delete app/application/models/config.py

--- app/application/models/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    ORACLE_HOST = 'localhost'  # Oracle数据库主机名
-    ORACLE_PORT = '1521'       # Oracle数据库端口
-    ORACLE_SID = 'orclpdb1'    # Oracle数据库服务名
-    ORACLE_USER = 'your_username'  # Oracle数据库用户名
-    ORACLE_PASSWORD = 'your_password'  # Oracle数据库密码
