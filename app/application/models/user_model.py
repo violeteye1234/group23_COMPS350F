a5fed9dfd8cb66827c45d40bb4c4097b5f793ef4
terry2778 <s1303276@live.hkmu.edu.hk>
2024-11-19 01:24:28 +0800

    Update user_model.py

--- app/application/models/user_model.py
+++ app/application/models/user_model.py
@@ -1,33 +1,46 @@
 import cx_Oracle
-from .config import Config
 
 class UserModel:
     def __init__(self):
-        dsn_tns = cx_Oracle.makedsn(Config.ORACLE_HOST, Config.ORACLE_PORT, service_name=Config.ORACLE_SID)
-        self.conn = cx_Oracle.connect(user=Config.ORACLE_USER, password=Config.ORACLE_PASSWORD, dsn=dsn_tns)
-        self.cursor = self.conn.cursor()
+        self.connection = None
+        self.cursor = None
+        self.set_database()
+
+    def set_database(self):
+        if not self.connection:
+            dsn = cx_Oracle.makedsn('oracleacademy.ouhk.edu.hk', 8998, sid='db1011')
+            self.connection = cx_Oracle.connect(user='s1305732', password='13057320', dsn=dsn)
+            self.cursor = self.connection.cursor()
 
     def add_user(self, full_name, phone, email, password):
+        self.set_database()
         query = "INSERT INTO Users (FullName, PhoneNumber, Email, PassWord) VALUES (:1, :2, :3, :4)"
         values = (full_name, phone, email, password)
         self.cursor.execute(query, values)
-        self.conn.commit()
+        self.connection.commit()
 
     def get_user_by_email(self, email):
+        self.set_database()
         query = "SELECT * FROM Users WHERE Email = :1"
         self.cursor.execute(query, (email,))
         return self.cursor.fetchone()
 
     def get_user_by_email_and_password(self, email, password):
+        self.set_database()
         query = "SELECT * FROM Users WHERE Email = :1 AND PassWord = :2"
         self.cursor.execute(query, (email, password))
         return self.cursor.fetchone()
 
     def update_password(self, email, new_password):
+        self.set_database()
         query = "UPDATE Users SET PassWord = :1 WHERE Email = :2"
         self.cursor.execute(query, (new_password, email))
-        self.conn.commit()
+        self.connection.commit()
 
     def close(self):
-        self.cursor.close()
-        self.conn.close()
+        if self.cursor:
+            self.cursor.close()
+        if self.connection:
+            self.connection.close()
+        self.connection = None
+        self.cursor = None
