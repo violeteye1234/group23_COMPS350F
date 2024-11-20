72b30471073a3092fe0e3e4d1e91793f2b96cdfb
kkkkkeven <1208914246@qq.com>
2024-11-20 20:02:03 +0800

    kk
    Implement recognition time

--- app/application/pages/notification_center/notification_center_controller.py
+++ app/application/pages/notification_center/notification_center_controller.py
@@ -1,9 +1,7 @@
 from utils.page_controller import PageController
 from .notification_center_view import NotificationCenterPageView
 from pages.my_baggage.my_baggage_controller import MyBaggagePageController
-#from utils.scrollable_frame import ScrollableFrame
-#import tkinter as tk
-
+from datetime import datetime, timedelta
 
 class NotificationCenterPageController(PageController):
     def __init__(self, root, parent_container):
@@ -11,3 +9,33 @@ class NotificationCenterPageController(PageController):
         self.root = root
         self.view = NotificationCenterPageView(parent_container)
         self.view_set_controller()
+
+        # 假设飞机起飞时间和降落时间
+        takeoff_time = datetime.strptime("18:00", "%H:%M")
+        landing_time = datetime.strptime("19:00", "%H:%M")
+        
+        current_time = datetime.now()
+
+        one_hour_ago = takeoff_time - timedelta(minutes=60)  # 行李检查 飞机起飞前一小时
+        half_hour_ago = takeoff_time - timedelta(minutes=30)  # 登机 飞机起飞前半小时
+        a_quarter_later = landing_time + timedelta(minutes=15)  # 行李到达 飞机降落后15分钟
+
+        time_until_takeoff = (takeoff_time - current_time).total_seconds() / 60
+        time_later_landing = (current_time - landing_time).total_seconds() / 60
+
+        current_time = current_time.strftime("%H:%M")
+        takeoff_time = takeoff_time.strftime("%H:%M")
+        landing_time = landing_time.strftime("%H:%M")
+        one_hour_ago = one_hour_ago.strftime("%H:%M")
+        half_hour_ago = half_hour_ago.strftime("%H:%M")
+        a_quarter_later= a_quarter_later.strftime("%H:%M")
+
+        # 将时间显示在控制器中
+        self.current_time = current_time
+        self.half_hour_ago = half_hour_ago
+        self.a_quarter_later = a_quarter_later
+        self.one_hour_ago = one_hour_ago
+        self.takeoff_time = takeoff_time
+        self.landing_time = landing_time
+        self.time_until_takeoff = time_until_takeoff
+        self.time_later_landing = time_later_landing
--- app/application/pages/notification_center/notification_center_view.py
+++ app/application/pages/notification_center/notification_center_view.py
@@ -27,27 +27,52 @@ class NotificationCenterPageView(PageView):
         original_image3 = Image.open(original_image_path3)
         resized_image3 = original_image3.resize(desired_size3)
         resized_image3.save(original_image_path3) # 覆盖原始图片
+       
+        MYBAGGAGE_check = f"Your Baggage is checking at {self.controller.one_hour_ago}                                                                        There is half an hour before boarding.                                                                         Please go through security as soon as possible."
+        MYBAGGAGE_arrive =f"Your Baggage arrived at {self.controller.a_quarter_later}                                                                                                    Please pick it up as soon as possible"
+        MYFLIGHT_boarding = f"Your Flight is boarding at {self.controller.half_hour_ago}                                                                                                    Please board the plane as soon as possible" 
 
-        x_position = 420
-        y_position = 120
-        
-        data = 'BFFBBFBBF'
-        MYBAGGAGE = "My Baggage"
-        MYFLIGHT="My Flight"
+        if self.controller.time_until_takeoff > 60:
+    # 代码块，处理时间大于60分钟的情况
+            i = 0
+        elif self.controller.time_until_takeoff <= 60 and self.controller.time_until_takeoff > 30:
+    # 代码块，处理时间介于30到60分钟之间的情况
+            i = 1
+        elif self.controller.time_until_takeoff <= 30 and self.controller.time_until_takeoff > 0:
+    # 代码块，处理时间介于0到30分钟之间的情况
+            i = 2
+        if self.controller.time_later_landing >= 15:
+    # 处理时间大于等于15分钟的情况
+            i= 3
 
-        for item in data:
-            if item == 'B':
-                button = CanvasButton(self.canvas, x_position, y_position, self.image_path / "image_3.png",
+        if i == 0:
+            label = tk.Label(self.canvas, text="No messages", font=("Arial", 16), bg="#F5F5F5")
+            label.place(x=420, y=120)
+        elif i == 1:
+            button1 = CanvasButton(self.canvas, 420,120, self.image_path / "image_3.png",
                                       lambda: self.controller.root.page_controller.switch_page("my_baggage"),
-                                      self.image_path / "image_2.png",text=MYBAGGAGE, font_size=16)
-                y_position += 100  # Increase Y position for the next button
-            elif item == 'F':
-                button = CanvasButton(self.canvas, x_position, y_position, self.image_path / "image_3.png",
+                                      self.image_path / "image_2.png",text=MYBAGGAGE_check, font_size=16)
+        elif i == 2:
+            button1 = CanvasButton(self.canvas, 420,220, self.image_path / "image_3.png",
+                                      lambda: self.controller.root.page_controller.switch_page("my_baggage"),
+                                      self.image_path / "image_2.png",text=MYBAGGAGE_check, font_size=16)
+
+            button2 = CanvasButton(self.canvas,420,120, self.image_path / "image_3.png",
                                       lambda: self.controller.root.page_controller.switch_page("my_flight"),
-                                      self.image_path / "image_2.png",text=MYFLIGHT, font_size=16)
-                y_position += 100  # Increase Y position
+                                      self.image_path / "image_2.png",text=MYFLIGHT_boarding, font_size=16)
+        if i == 3:
+            button1 = CanvasButton(self.canvas, 420,320, self.image_path / "image_3.png",
+                                      lambda: self.controller.root.page_controller.switch_page("my_baggage"),
+                                      self.image_path / "image_2.png",text=MYBAGGAGE_check, font_size=16)
 
+            button2 = CanvasButton(self.canvas,420,220, self.image_path / "image_3.png",
+                                      lambda: self.controller.root.page_controller.switch_page("my_flight"),
+                                      self.image_path / "image_2.png",text=MYFLIGHT_boarding, font_size=16)
 
+            button3 = CanvasButton(self.canvas, 420,120, self.image_path / "image_3.png",
+                                      lambda: self.controller.root.page_controller.switch_page("my_baggage"),
+                                      self.image_path / "image_2.png",text=MYBAGGAGE_arrive, font_size=16)
+        
         self.parent.set_frame(self)
 
         
